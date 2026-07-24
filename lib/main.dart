7d6912a443664957d4fb76de3146581a0afff036
9602894 <9602894@qq.com>
2026-07-25 07:55:32 +0800

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/home_screen.dart';
+import 'services/settings_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => SettingsService(),
+      child: MaterialApp(
+        title: 'Witv',
+        theme: ThemeData.dark(),
+        home: HomeScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
