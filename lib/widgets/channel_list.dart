3b54a95cc481ac361feb8c6c59fba53694623dd3
9602894 <9602894@qq.com>
2026-08-01 00:12:43 +0800

    Update channel_list.dart

--- lib/widgets/channel_list.dart
+++ lib/widgets/channel_list.dart
@@ -20,15 +20,6 @@ class ChannelList extends StatelessWidget {
     this.showLogo = true,
   }) : super(key: key);
 
-  // 工具：获取当前 UTC 时间
-  DateTime _getUtcNow() => DateTime.now().toUtc();
-
-  // 工具：格式化 UTC 时间为北京时间（+8）的 HH:mm
-  String _formatBeijingTime(DateTime utcTime) {
-    final beijing = utcTime.add(Duration(hours: 8));
-    return '${beijing.hour.toString().padLeft(2, '0')}:${beijing.minute.toString().padLeft(2, '0')}';
-  }
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -39,10 +30,10 @@ class ChannelList extends StatelessWidget {
         final epgList = epgMap[ch.name] ?? [];
         String? currentTitle;
         if (epgList.isNotEmpty) {
-          final now = _getUtcNow();
+          final now = DateTime.now(); // 设备本地时间（北京时间）
           for (var prog in epgList) {
             if (prog.start.isBefore(now) && prog.end.isAfter(now)) {
-              currentTitle = '${_formatBeijingTime(prog.start)}-${_formatBeijingTime(prog.end)} ${prog.title}';
+              currentTitle = '${prog.start.hour.toString().padLeft(2, '0')}:${prog.start.minute.toString().padLeft(2, '0')}-${prog.end.hour.toString().padLeft(2, '0')}:${prog.end.minute.toString().padLeft(2, '0')} ${prog.title}';
               break;
             }
           }
